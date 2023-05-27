d20efbde4ba4874f902ce7e0c43f223abef5c763
Onuorah2 <133059532+Onuorah2@users.noreply.github.com>
2023-05-27 08:16:11 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-
+SQLALCHEMY_DATABASE_URL = postgres://myapi_4jxf_user:uwRA2kJKk9AQCBT3NuDx86Q0lK4xTvV2@dpg-choqm6m7avjb90itejgg-a/myapi_4jxf
 engine = create_engine(SQLALCHEMY_DATABASE_URL)#this is the engine responsible for establishing a aconnection to talk with the database. to connect with the database and then you pass in the connection string
 
 Sessionlocal = sessionmaker(autocommit= False, autoflush= False, bind=engine)#when you want to talk with the database, u have to make use of a session
